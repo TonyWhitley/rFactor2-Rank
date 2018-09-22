cebfcec150fb19fda76cca514a384c52f05241e1
TonyWhitley <Tony_Whitley@hotmail.com>
2018-09-22 12:24:54 +0100

    Using a generator that gives one complete PLAYERTRACKSTAT
    I thought it might make things clearer by "chunking" the file into PLAYERTRACKSTATs. Oh well.

--- create_file.py
+++ create_file.py
@@ -11,9 +11,46 @@ problems, feel free to contact us at isiforums.net (user "hoover").
 from __future__ import print_function  # Python 2 compatibility
 
 import glob,re,sys
-version="0.27t"
+version="0.27t (generator)"
 
 maxErrorLineLength = 60 # Long entries in the cch file spoil the error message appearance
+
+#iterator (not working, replaced with generator)
+class IplayerTrackStat:
+  def __init__(self, trackstats):
+    self.trackstats = iter(trackstats)
+  def __iter__(self):
+    self._next = next(self.trackstats)
+    return self
+  def next(self):
+    if self._next[1] == '[PLAYERTRACKSTAT]':
+      _trackstat = self._next
+      self._next = next(self.trackstats)
+      while self._next != '[PLAYERTRACKSTAT]':
+        _trackstat.append(self._next)
+        self._next = next(self.trackstats)
+      yield _trackstat
+
+def playerTrackStat(trackstats):
+  """ generator that gives one complete PLAYERTRACKSTAT """
+  def getOneTrackstat(_trackstats, _first):
+    _trackstat = [_first] # [linenum, '[PLAYERTRACKSTAT]']
+    _next = next(_trackstats)
+    try:
+      while not _next[1].startswith('[PLAYERTRACKSTAT]'):
+        _trackstat.append(_next)
+        _next = next(_trackstats)
+    except StopIteration:
+      pass
+    return _trackstat, _next
+
+  _trackstats = iter(trackstats)
+  _first = next(_trackstats)
+  _trackstat, _first = getOneTrackstat(_trackstats, _first)
+  while True:
+    yield _trackstat
+    _trackstat,_first = getOneTrackstat(_trackstats, _first)
+  yield _trackstat
 
 def get_trackstats(file="career.txt"):
     """
@@ -21,6 +58,9 @@ def get_trackstats(file="career.txt"):
     Has rudimentary error checking
     Returns: Array containing valid PLAYERTRACKSTAT sections
     """
+    def error(file, trackstat, originalFile):
+      pass
+
     wanted_lines = [
       '[PLAYERTRACKSTAT]',
       'TrackName',
@@ -33,56 +73,46 @@ def get_trackstats(file="career.txt"):
         print("couldn't open file %s" % file)
         sys.exit(1)
 
-    data = fh.readlines()
+    originalFile = []
+    _trackstats = []
+    for linecount, line in enumerate(fh):
+      originalFile.append(line)
+      for keyword in wanted_lines:
+        if line.startswith(keyword):
+          _trackstats.append((linecount, line))
+          break # (keyword matched, no need to keep searching)
 
-    if 0:
-      #simple
-      trackstats = []
-      for linecount, line in enumerate(data):
-        for keyword in wanted_lines:
-          if line.startswith(keyword):
-            trackstats.append(line)
-            break
-    else:
-      # with error checking
-      trackstats = []
-      trackstatBlocks = []
-      for linecount, line in enumerate(data):
-          if line.startswith('[PLAYERTRACKSTAT]'):
-            trackstatBlocks.append(linecount)
-      trackstatBlocks.append(len(data)) # the last one continues to the end of the file
-         
-      for block in range(len(trackstatBlocks)-1):
-        trackstat = []
-        for line in data[trackstatBlocks[block]:trackstatBlocks[block+1]]:
-          for keyword in wanted_lines:
-            if line.startswith(keyword):
-              trackstat.append(line)
-              break
-        # sanity check for keywords TrackName, ClassRecord
+    results = []
+    # Error checking
+    z = playerTrackStat(_trackstats)
+    _trackstatsList = list(z)
+    for trackstat in _trackstatsList:
+        # sanity check for keywords TrackName, TrackFile, ClassRecord
         if len(trackstat) >= 4:
-          if trackstat[0].startswith(wanted_lines[0]) and \
-          trackstat[1].startswith(wanted_lines[1]) and \
-          trackstat[2].startswith(wanted_lines[2]):
+          if trackstat[0][1].startswith(wanted_lines[0]) and \
+          trackstat[1][1].startswith(wanted_lines[1]) and \
+          trackstat[2][1].startswith(wanted_lines[2]):
             #it's good
-            trackstats.extend(trackstat)
+            for line in trackstat:
+              results.append(line[1])
             continue
         # it's bad
-        err_line = trackstatBlocks[block]+1
+        err_line = trackstat[0][0]+1
         print("""
 WARNING: Something's wrong with a PLAYERTRACKSTAT entry
 in file %s at line %d, please check.
 Keywords "TrackName" and / or "ClassRecord" not found
 where I expected them:
         """ % (file, err_line))
-        for line in data[trackstatBlocks[block]:trackstatBlocks[block+1]]:
+        _lastline = trackstat[-1][0]+1 # the linenum of the last line in this trackstat
+        for line in originalFile[trackstat[0][0]:_lastline]:
           if len(line) > maxErrorLineLength:
             line = line[:maxErrorLineLength] + '...\n'
           print("%s:%d: %s" % (file, err_line, line), end='')
           err_line += 1
         print()
 
-    return trackstats
+    return results
 
 def create_file(filename="career.blt"):
     """Create a career.blt file from CCH files found in the current directoy"""
